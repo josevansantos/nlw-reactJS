07c4b6979db9370ade1204e918e2c4319eca2d3c
Josevan Santos <josevansnt@gmail.com>
2021-09-28 15:38:59 -0300

    feat: create routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,44 @@
+import { useHistory } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg'
+import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
-import '../styles/auth.scss'
+import '../styles/auth.scss';
 
-export function Home () {
+export function Home() {
+  const history = useHistory();
+
+  function navegateToNewRoom() {
+    history.push('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas de sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div className='main-content'>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className='create-room'>
+          <button onClick={navegateToNewRoom} className="create-room">
             <img src={googleIconImg} alt="logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div className='separator'>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
-            <input
-              type='text'
-              placeholder='Digite o código da sala'
-            />
-            <Button type='submit'>
-              Entra na sala
-            </Button>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entra na sala</Button>
           </form>
         </div>
       </main>
     </div>
-  )
+  );
 }
